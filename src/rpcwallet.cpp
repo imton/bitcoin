9a0930a7a91ba48707a448cf6add9e4ec2631661
Gaston M <ton@imton.me>
2013-12-08 14:43:34 -0200

    ok…

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -3,8 +3,6 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-
-
 #include "base58.h"
 #include "bitcoinrpc.h"
 #include "init.h"
